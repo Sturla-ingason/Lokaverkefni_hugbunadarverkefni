d392b740ee55f96e23f9c91169b44c91c40b967a
Sturla-ingason <Sturladi@gmail.com>
2025-09-30 14:10:04 +0000

    userController setur upp
    Setti upp user controller og er að byrja að mappa út user service!

---
+++ src/main/java/mainFiles/Service/UserService.java
@@ -0,0 +1,57 @@
+package mainFiles.Service;
+
+import org.springframework.stereotype.Service;
+
+import mainFiles.Data.FollowerData;
+import mainFiles.Data.UserData;
+
+@Service
+public class UserService{
+    private UserData userData;
+    private FollowerData followerData;
+
+    public void save() {
+        
+    }
+
+    
+    public void deleate(){
+
+    }
+
+    public void updateEmail(){
+
+    }
+
+
+    public void updateUsername(){
+
+    }
+
+
+    public void updatePassword(){
+
+    }
+
+
+    public void resetpassword(){
+
+    }
+
+
+    public void follow(){
+
+    }
+
+
+    public void unfollow(){
+
+    }
+
+
+    public void getFollowerAmountU(){
+
+    }
+
+
+}
