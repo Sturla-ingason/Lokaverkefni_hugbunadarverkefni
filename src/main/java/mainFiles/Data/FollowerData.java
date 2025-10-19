7652725e807b95ab000a1e852b79c475d581f484
olafurkristofer <olafur.k123@gmail.com>
2025-10-19 19:49:15 +0000

    Laga follower repository

--- src/main/java/mainFiles/Data/FollowerData.java
+++ src/main/java/mainFiles/Data/FollowerData.java
@@ -8,16 +8,9 @@ import java.util.List;
 @Repository
 public interface FollowerData{
 
-    //TODO be able to follow another user.
-
-    //TODO be able to unfollow a user.
-
-    //TODO be able to get all the followers of a user.
-
-    //TODO be able to get all the users a user is following.
-
-    //TODO get the follower amount of a user.
-
-    //TODO get the following amount of a user.
+    void followUser(Integer userID, Integer followerID);
+    void unfollowUser(Integer userID,  Integer followerID);
+    List<User> getAllFollowedByUser(Integer userID);
+    Integer getAmountOfFollowers(Integer userID);
 
 }
