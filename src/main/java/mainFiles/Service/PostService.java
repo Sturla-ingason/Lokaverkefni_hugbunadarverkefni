726d18d3636346e3c29b309a60850e1055be8683
JAO300 <jonadal02@gmail.com>
2025-10-21 03:55:09 +0000

    like og unlike

--- src/main/java/mainFiles/Service/PostService.java
+++ src/main/java/mainFiles/Service/PostService.java
@@ -1,7 +1,62 @@
 package mainFiles.Service;
 
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import mainFiles.Data.PostData;
+import mainFiles.objects.Post;
+
+@Service
 public class PostService {
 
+    private final PostData postData;
 
+    public PostService(PostData postData) {
+        this.postData = postData;
+    }
 
+    /**
+     * Add a like to a post from the user
+     *
+     * @param postId 
+     * @param userId 
+     */
+    @Transactional
+    public void likePost(Integer postId, Integer userId) {
+        
+        //fetch post
+        Post post = postData.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("Post not found"));
+
+        //add user to posts list of likes
+        boolean added = post.addLike(userId);
+        if (!added) {
+            throw new IllegalStateException("User already liked this post");
+        }
+
+        //save 
+        postData.save(post);
+    }
+
+    /**
+     * Remove a like from a post from the user
+     *
+     * @param postId 
+     * @param userId 
+     */
+    @Transactional
+    public void unlikePost(Integer postId, Integer userId) {
+        
+        //fetch post
+        Post post = postData.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("Post not found"));
+        
+        //remove user from posts list of likes
+        boolean removed = post.removeLike(userId);
+        if (!removed) {
+            throw new IllegalStateException("User has not liked this post");
+        }
+
+        //save
+        postData.save(post);
+    }
 }
--- src/main/java/mainFiles/controllers/PostController.java
+++ src/main/java/mainFiles/controllers/PostController.java
@@ -1,9 +1,38 @@
 package mainFiles.controllers;
+import mainFiles.Service.PostService;
+import mainFiles.objects.Post;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import mainFiles.Service.PostService;
+
+@RestController
 public class PostController {
+
+
+    private final PostService postService;
+
+    public PostController(PostService postService){
+      this.postService = postService;
+    }
 
      public void createNewPost(){
        //TODO
     }
+
+
+    @PostMapping("/{postId}/like")
+    public void likePost(@PathVariable Integer postId, @RequestParam Integer userId) {
+        postService.likePost(postId, userId);
+    }
+
+    @DeleteMapping("/{postId}/like")
+    public void unlikePost(@PathVariable Integer postId, @RequestParam Integer userId) {
+        postService.unlikePost(postId, userId);
+    }
+
 
 }
