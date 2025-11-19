fd3d7a82687330a3735d4ae0b918377ca825c6fb
JAO300 <jonadal02@gmail.com>
2025-11-19 23:01:05 +0000

    delete post fix og like display a feed

--- src/main/java/mainFiles/Data/CommentData.java
+++ src/main/java/mainFiles/Data/CommentData.java
@@ -3,8 +3,10 @@ package mainFiles.Data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import mainFiles.objects.Comment;
+import mainFiles.objects.Post;
 
 @Repository
 public interface CommentData extends JpaRepository<Comment, Integer> {
     
+    void deleteByPost(Post post);
 }
